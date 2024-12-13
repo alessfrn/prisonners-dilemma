8f93058daf92709c73b9ac35ed40c713f62f9be9
l3miage-farinaa <alessandro.farina@etu.univ-grenoble-alpes.fr>
2024-12-13 18:29:42 +0100

    Updated pom & config

--- src/main/java/fr/uga/l3miage/pc/App.java
+++ src/main/java/fr/uga/l3miage/pc/App.java
@@ -2,8 +2,9 @@ package fr.uga.l3miage.pc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
